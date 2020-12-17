a017029e06fcfec07b120d71cf888942a1af8e53
Suman Barick <codotronix@gmail.com>
2020-12-17 20:30:19 +0530

    playground started

--- gatsby-src/src/pages/playground/index.js
+++ gatsby-src/src/pages/playground/index.js
@@ -1,10 +1,40 @@
 import React from 'react'
+// import { Link } from "gatsby"
+import { Layout } from '../../components/common'
+import { Container, Grid, Box, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    editor: {
+
+        '& .writearea': {
+
+        }
+    }
+})
 
 const Playground = props => {
+    const classes = useStyles()
+
     return (
-        <div>
-            Welcome to Playground
-        </div>
+        <Layout>
+            <Container maxWidth="lg">
+                <Box
+                    mb={8}
+                >
+
+
+                <Grid container spacing={1}>
+                    <Grid item xs={12} md={6}>
+                        <div className={classes.editor}>
+                            <textarea className="writearea"></textarea>
+                        </div>
+                        
+                    </Grid>
+                </Grid>
+                    
+                </Box>
+            </Container>
+        </Layout>
     )
 }
 
